5d2a02d0c319560b05fea3392834f5a73550579d
Travis Chen <travis.chen@everchanging.dev>
2020-03-04 20:30:34 -0800

    modules/elliptical_path.bash: include more of the path
    Instead of only printing the current directory when the path is longer than
    desired, we will print everything before the current directory up to the
    desired length. This way, we can see more of the path.

--- modules/elliptical_path.bash
+++ modules/elliptical_path.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 elliptical_path() {
-    if (($(pwd | sed "s|^$HOME||" | wc -c)
-         > ${PROMPT_FRAMEWORK_ELLIPTICAL_PATH_LENGTH:-40})); then
-        echo ".../\W"
+    local path=$(pwd | sed "s|^$HOME|~|")
+    local length=${PROMPT_FRAMEWORK_ELLIPTICAL_PATH_LENGTH:-40}
+
+    if (($(echo "$path" | wc -c) > $length)); then
+        echo "...${path:$((${#path} - $length + 3)):$(($length - 3))}"
     else
-        echo "\w"
+        echo "$path"
     fi
 }
 
