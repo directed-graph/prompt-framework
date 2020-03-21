f5d79b87560db91c29f80a54e89938ba4647783f
Travis Chen <travis.chen@everchanging.dev>
2020-03-21 15:33:43 -0700

    modules/date_time.bash: added

---
+++ modules/date_time.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Prints the date and time. Can be turned on and off as needed.
+#
+# Environment Variables:
+#   PF_DATE_TIME: whether or not to enable (default: no)
+#   PF_DATE_TIME__FORMAT: the format for strftime (default: %F %T)
+date_time() {
+    if [[ "${PF_DATE_TIME:-no}" == "yes" ]]; then
+        echo "[\D{${PF_DATE_TIME__FORMAT:-%F %T}}]"
+    fi
+}
+
