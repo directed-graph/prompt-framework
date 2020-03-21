5caef957f581ff4a6333206fdb7ef3025da9a821
Travis Chen <travis.chen@everchanging.dev>
2020-03-21 15:08:17 -0700

    modules/{elliptical_path,host_name}.bash: update variable names

--- modules/elliptical_path.bash
+++ modules/elliptical_path.bash
@@ -2,7 +2,7 @@
 
 elliptical_path() {
     local path=$(pwd | sed "s|^$HOME|~|")
-    local length=${PROMPT_FRAMEWORK_ELLIPTICAL_PATH_LENGTH:-40}
+    local length=${PF_ELLIPTICAL_PATH__LENGTH:-40}
 
     if (($(echo "$path" | wc -c) > $length)); then
         echo "...${path:$((${#path} - $length + 3)):$(($length - 3))}"
--- modules/exit_code.bash
+++ modules/exit_code.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 exit_code() {
-    if ((${EXIT_CODE:-0} != 0)); then
+    if ((${PF_EXIT_CODE:-0} != 0)); then
         local color_red="\[\e[0;31m\]"
         local color_off="\[\033[0m\]"
-        echo "(${color_red}${EXIT_CODE}${color_off}) "
+        echo "(${color_red}${PF_EXIT_CODE}${color_off}) "
     fi
 }
 
